dcc334af8cce47e3b260be7644c5803266ea411f
Ulukman Zhamshitov <uakmatof@gmail.com>
2021-03-24 10:04:57 -0500

    assignment

---
+++ shows_app/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+from . import views
+urlpatterns = [
+    path('', views.index),
+    path('shows/new', views.new),
+    path('shows/create', views.create),
+    path('shows/<int:show_id>', views.about),
+    path('shows', views.shows),
+    path('shows/<int:show_id>/edit', views.edit),
+    path('shows/<int:show_id>/update', views.update),
+    path('shows/<int:show_id>/destroy', views.delete),
+
+]
---
+++ shows_app/views.py
@@ -0,0 +1,65 @@
+from django.shortcuts import render, redirect
+from .models import Show
+def index (request):
+    return redirect ('/shows')
+    
+def new (request):
+    context = {
+        'all_shows': Show.objects.all()
+    }
+    return render(request, "index.html", context)
+
+def create(request):
+    show = Show.objects.create(
+        title = request.POST['title'],
+        network = request.POST['network'],
+        description = request.POST['desc'],
+        release_date = request.POST['date']
+    )
+    show_id = show.id
+    return redirect(f'/shows/{show_id}')
+
+def about(request, show_id):
+    show = Show.objects.get(id=show_id)
+    context = {
+        'id': show.id,
+        'title': show.title,
+        'network': show.network,
+        'date': show.release_date,
+        'desc': show.description,
+        'update_date': show.updated_at
+    }
+    return render(request, "about.html", context)
+
+def shows(request):
+    context = {
+        'all_shows': Show.objects.all()
+    }
+    return render(request, "shows.html", context)
+
+def edit(request, show_id):
+    show = Show.objects.get(id=show_id)
+    context = {
+        'id': show.id,
+        'title': show.title,
+        'network': show.network,
+        'date': show.release_date,
+        'desc': show.description,
+        'update_date': show.updated_at
+    }
+    return render(request, "edit.html", context)
+
+def update(request, show_id):
+    show = Show.objects.get(id=show_id)
+    show.title = request.POST['title']
+    show.network = request.POST['network']
+    show.description = request.POST['desc']
+    show.release_date = request.POST['date']
+    show.save()
+    return redirect(f'/shows/{show_id}')
+
+def delete(request, show_id):
+    show = Show.objects.get(id=show_id)
+    show.delete()
+    return redirect('/shows')
+
